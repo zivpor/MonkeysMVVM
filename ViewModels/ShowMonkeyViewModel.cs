5b62c02a2e919c1fe14ab3ac6eff78485cdf952d
User <User@LAB3-3>
2024-01-29 12:42:05 +0200

    Add project files.

---
+++ ViewModels/ShowMonkeyViewModel.cs
@@ -0,0 +1,62 @@
+﻿using MonkeysMVVM.Models;
+using MonkeysMVVM.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeysMVVM.ViewModels
+{
+    public class ShowMonkeyViewModel:INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        private string name;
+        public string Name 
+        { 
+            get { return this.name; } 
+            set
+            {
+                this.name = value;
+                OnPropertyChanged();
+            } 
+        }
+        private string location;
+        public string Location
+        {
+            get { return this.location; }
+            set
+            {
+                this.location = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string imageUrl;
+        public string ImageUrl
+        {
+            get { return this.imageUrl; }
+            set
+            {
+                this.imageUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+      
+
+     
+        
+    }
+}
